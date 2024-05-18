18a4687c5183b7a6282284232816213d1abbc9e3
Sayantan <sayantann2002@gmail.com>
2024-05-19 04:06:02 +0530

    added playground

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import express from 'express';
 import { schema } from './graphql';
 import { createContext } from './context';
 import config from './config';
+import expressPlayground  from 'graphql-playground-middleware-express';
 
 const app = express();
 
@@ -15,8 +16,12 @@ const server = new ApolloServer({
   formatError: (err) => ({ message: err.message, status: 400 }),
 });
 
+
 server.start().then(() => {
-  server.applyMiddleware({ app });
+  server.applyMiddleware({
+    app
+  });
+  app.get('/playground', expressPlayground({ endpoint: '/graphql' }));
   app.listen({ port: config.port }, () =>
     console.log(`🚀 Server ready at http://localhost:${config.port}${server.graphqlPath}`)
   );

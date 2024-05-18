f28419de32b45144b917b27ad884f0bfb423d940
Sayantan <sayantann2002@gmail.com>
2024-05-19 04:48:00 +0530

    added static html

--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,13 @@ import express from 'express';
 import { schema } from './graphql';
 import { createContext } from './context';
 import config from './config';
-import expressPlayground  from 'graphql-playground-middleware-express';
+import path from 'path';
+import expressPlayground from 'graphql-playground-middleware-express';
 
 const app = express();
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../public')));
 
 const server = new ApolloServer({
   schema,
@@ -22,6 +24,12 @@ server.start().then(() => {
   server.applyMiddleware({
     app
   });
+
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+  });
+
+
   app.get('/playground', expressPlayground({ endpoint: '/graphql' }));
   app.listen({ port: config.port }, () =>
     console.log(`🚀 Server ready at http://localhost:${config.port}${server.graphqlPath}`)
